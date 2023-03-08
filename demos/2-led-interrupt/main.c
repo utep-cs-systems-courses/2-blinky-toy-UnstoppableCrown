29404e23dfad9d67851806ae3df1134a2248ec48
Jacob <jagonzalez63@miners.utep.edu>
2023-03-08 11:25:46 -0700

    First three demos completed

--- demos/2-led-interrupt/main.c
+++ demos/2-led-interrupt/main.c
@@ -5,8 +5,8 @@
 
 int main(void) {
   P1DIR |= LEDS;
-  P1OUT &= ~LED_GREEN;
-  P1OUT |= LED_RED;
+  P1OUT &= ~LED_RED;
+  P1OUT |= LED_GREEN;
 
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
   enableWDTInterrupts();	/* enable periodic interrupt */
@@ -14,10 +14,16 @@ int main(void) {
   or_sr(0x18);		/* CPU off, GIE on */
 }
 
+int led_on = 0;
 
 void
 __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
 {
-  P1OUT |= LED_GREEN;
+  if(led_on == 1250) {
+    P1OUT |= LED_RED;
+    P1OUT &= ~LED_GREEN;
+  }
+  
+  led_on++;
 } 
 
